3ba14358dec436828f6494aa32562d7f0a0e6df9
Tu TV <tutv95@gmail.com>
2015-02-17 18:45:16 +0700

    Phát hiện link lỗi :3

--- php/solution.php
+++ php/solution.php
@@ -1,49 +1,52 @@
 <?php
 if (!empty($_GET["url"])) {
     $url = $_GET["url"];
-    if (strpos($url, "ttp://") == null || strpos($url, "zing.vn") == null) {
-        echo "<h3 class='list-group-item list-group-item-danger'>Link không hợp lệ <img src='img/cry.png'/></h3>";
+    if (strpos($url, "ttp://") == null || strpos($url, "p3.zing.vn") == null) {
+        echo printError();
+        return;
     }
-    else {
-        if (strpos($url, "://mp3") != null) {
-            $arr = explode("http://", $url);
-            $urlm = "http://m." . $arr[1];
-        } else if (strpos($url, "://m.mp3.zing.vn") != null) {
-            $urlm = $url;
-        }
-
-        if ($content = file_get_contents($urlm)) {
-            //Get img ava
-            $content1 = explode("property=\"og:image\" content=\"", $content)[1];
-            $img = explode("\"", $content1)[0];
-
-            //Get lyric
-            $lyric = explode("<p id=\"conLyrics\" class=\"row-5\">", $content)[1];
-            $lyric = explode("</p>", $lyric)[0];
-
-            //Get infomation song
-            $content = explode("xml=\"", $content)[1];
-            $xmlURL = explode("\"", $content)[0];
-            $jsonContent = file_get_contents($xmlURL);
-            $music = json_decode($jsonContent);
-            $data = $music->data[0];
-
-            //Content
-            $title = $data->title;
-            $singer = $data->performer;
-            $link128 = $data->source;
-            $link320 = "http://mp3.zing.vn/download/vip/song/" . getID($url);
-
-            echo "<li class=\"list-group-item list-group-item-info\"><h3 style=\"font-weight: bold;\">" . $title . "</h3></li>";
-            echo "<li class=\"list-group-item list-group-item-info\">Thể hiện: <strong>" . $singer . "</strong></li>";
-            echo "<li class=\"list-group-item list-group-item-info\">Download: <a href=\"" . $link128 . "\" download=\"" . $link128 . "\">128 kpbs</a> - <a href=\"" . $link320 . "\" download=\"" . $link320 . "\">320 kpbs</a></li>";
-            echo "<li class=\"list-group-item list-group-item-info\"><button class=\"btn btn-info\" id=\"btn-play\" onclick=\"playMusic();\">Play</button><audio controls style=\"display: none;\" id=\"player\"><source src=\"" .  $link128 ."\" type=\"audio/mpeg\">Your browser does not support the audio element.</audio></li>";
-            echo "<li class=\"list-group-item list-group-item-warning\"><p>" . $lyric . "</p></li>";
-        }
-        else {
-            echo "<h3 class='list-group-item list-group-item-danger'>Đã có lỗi xảy ra <img src='img/cry.png'/></h3>";
-        }
+    if (strpos($url, "://mp3") != null) {
+        $arr = explode("http://", $url);
+        $urlm = "http://m." . $arr[1];
+    } else if (strpos($url, "://m.mp3.zing.vn") != null) {
+        $urlm = $url;
     }
+
+    if (!($content = file_get_contents($urlm))) {
+        echo printError();
+        return;
+    }
+    //Get infomation song
+    $info = explode("xml=\"", $content);
+    if ($info[0] == $content) {
+        echo printError();
+        return;
+    }
+    $info = $info[1];
+    $xmlURL = explode("\"", $info)[0];
+    $jsonContent = file_get_contents($xmlURL);
+    $music = json_decode($jsonContent);
+    $data = $music->data[0];
+
+    //Get img ava
+    //$content1 = explode("property=\"og:image\" content=\"", $content)[1];
+    //$img = explode("\"", $content1)[0];
+
+    //Get lyric
+    $lyric = explode("<p id=\"conLyrics\" class=\"row-5\">", $content)[1];
+    $lyric = explode("</p>", $lyric)[0];
+
+    //Content
+    $title = $data->title;
+    $singer = $data->performer;
+    $link128 = $data->source;
+    $link320 = "http://mp3.zing.vn/download/vip/song/" . getID($url);
+
+    echo "<li class=\"list-group-item list-group-item-info\"><h3 style=\"font-weight: bold;\">" . $title . "</h3></li>";
+    echo "<li class=\"list-group-item list-group-item-info\">Thể hiện: <strong>" . $singer . "</strong></li>";
+    echo "<li class=\"list-group-item list-group-item-info\">Download: <a href=\"" . $link128 . "\" download=\"" . $link128 . "\">128 kpbs</a> - <a href=\"" . $link320 . "\" download=\"" . $link320 . "\">320 kpbs</a></li>";
+    echo "<li class=\"list-group-item list-group-item-info\"><button class=\"btn btn-info\" id=\"btn-play\" onclick=\"playMusic();\">Play</button><audio controls style=\"display: none;\" id=\"player\"><source src=\"" .  $link128 ."\" type=\"audio/mpeg\">Your browser does not support the audio element.</audio></li>";
+    echo "<li class=\"list-group-item list-group-item-warning\"><p>" . $lyric . "</p></li>";
 }
 else {
     die("No!");
@@ -54,5 +57,9 @@ function getID($url) {
     $arr = explode("/", $url);
     $pos = count($arr) - 2;
     return $arr[$pos];
+}
+
+function printError() {
+    return "<h3 class='list-group-item list-group-item-danger'>Đã có lỗi xảy ra <img src='img/cry.png'/></h3>";
 }
 ?>
